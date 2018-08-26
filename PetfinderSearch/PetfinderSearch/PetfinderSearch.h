f07ac3a096f07f0d19b661951025c3f4bd36c10d
joesus <joesusnick@gmail.com>
2018-08-26 16:35:38 -0600

    Adding project

---
+++ PetfinderSearch/PetfinderSearch/PetfinderSearch.h
@@ -0,0 +1,19 @@
+//
+//  PetfinderSearch.h
+//  PetfinderSearch
+//
+//  Created by Joe Susnick on 8/26/18.
+//  Copyright © 2018 Joe Susnick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PetfinderSearch.
+FOUNDATION_EXPORT double PetfinderSearchVersionNumber;
+
+//! Project version string for PetfinderSearch.
+FOUNDATION_EXPORT const unsigned char PetfinderSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PetfinderSearch/PublicHeader.h>
+
+
